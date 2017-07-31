bf87163b3d20af94481fce62a0106f3fa8a2baf8
dctelf <david.telfer@gmail.com>
2017-07-31 08:12:50 +0100

    added scripts, updated readme, restructure modules, created test lcoation

---
+++ modules/stringmanip.py
@@ -0,0 +1,12 @@
+# /usr/bin/python3
+
+def hexStringToByteArray(hex_string):
+    return ("abc123")
+
+
+def bytearrayToBase64(bytearray):
+    return ("def456")
+
+
+def hexToBase64(hex_string):
+    return bytearrayToBase64(hexStringToByteArray(hex_string))
