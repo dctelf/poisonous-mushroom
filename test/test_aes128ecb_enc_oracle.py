ca364678dfb7095d1be530c82f44a110f056030b
dctelf <david.telfer@gmail.com>
2017-09-22 19:08:26 +0100

    completed the aes128 sliding window Oracle attack - code could do with a tidy and it's in a test method rather than being pulled out into a script or set of functions.  Needs tidied up.

--- test/test_aes128ecb_enc_oracle.py
+++ test/test_aes128ecb_enc_oracle.py
@@ -11,11 +11,21 @@ class TestAes128ecb_enc_oracle(TestCase):
 
     def test_decryptSuffix(self):
 
+        # initialise the global encryption key for all operations in this run
         config.oracle_enc_key = ciphers.generate_rand_ba(16)
+
+        # call the oracle with an empty byte array, i.e. get only the ciphertext of the unknown suffix back
         blank_ctext = ciphers.aes128ecb_enc_oracle(bytearray())
         blank_ctlen = len(blank_ctext)
 
-        # rather than do a while True and have it run off forever by accident, create a range up to 1000
+
+        # nudge an ever lengthening string of "A"'s into the oracle
+        # at some length of "A"'s the oracle function will snap into another block
+        # the moment this happens, we take the diference in lengths to determine what the marginal increase
+        # in length of ctext is.
+        # this difference is the blocklength of the oracle function
+        #
+        # (rather than do a while True and have it run off forever by accident, create a range up to 1000)
 
         for i in range(1000):
             test_block_ba = bytearray(i * "A", 'utf-8')
@@ -25,67 +35,109 @@ class TestAes128ecb_enc_oracle(TestCase):
                 break
 
         # now detect ECB by creating ptext of 4 * blocklen
+        # "4 *" used to avoid any pre or post padding of the plaintext passed (assumes any padding does not extend
+        # beyond a single block at either end - if it did , this wouldn't be a representative oracle function
+        # there wil lbe at least 2 "sandwidched" middle blocks of plaintext with which to compare
 
         test_ptext = bytearray(4 * blocklen * "A", 'utf-8')
         ctext = ciphers.aes128ecb_enc_oracle(test_ptext)
         if ctext[blocklen:2*blocklen] != ctext[2*blocklen:3*blocklen]: exit("not ecb mode")
 
-        # need to create a data structure of prior blocks so we don't lose
-        # the chain of identified characters as the prepend string shortens
-        ## NOTE: as each ctext block is independent of each other, this entire sequence to derive a block
-        ## can be iterated over for "ctextlen // blocklen", that is we can solve for a block at a time
-        ## need to  be slightly cautious though as the full ctext len will flip +/- by 1 block
-        ## as we prefix or suffix the prepend string of A's
-        ##
-        # ctext block will have been generated from ptext block of
-        # (blocklen - 1)* "A" + ?
-        # cyle across (blocklen - 1)* "A" + [0-255] and encrypt
-        # find matching full block
-        # we know unknown byte 0 is the result of the cycle - call this Q'
-        #
-        # next ctext block will have been generated from ptext block of
-        # (blocklen - 2)* "A" + Q' + ?
-        # cyle across (blocklen - 2)* "A" + Q' + [0-255] and encrypt
-        # find matching full block
-        # we know unknown byte 1 is the result of the cycle - call this Q''
-        #
-        # next ctext block will have been generated from ptext block of
-        # (blocklen - 3)* "A" + Q' + Q'' + ?
-        # cyle across (blocklen - 2)* "A" + Q' + Q'' + [0-255] and encrypt
-        # find matching full block
-        # we know unknown byte 2 is the result of the cycle - call this Q'''
-        # etc.
 
+        # also of note: once the first block is solved, we just cycle the loop again
+        # but instead of varying the last byte at the end of the "A"s, we use "A"s to nudge the oracle function back one
+        # then create a string of, say, 15 of our just solved block 0 characters, plus 1 0-255 "to find" chars....
+        # e.g. with a blocklen of 4 bytes (to ease illustration)
+        # oracle encrypts:
+        # {input bytes}[XBCD][EFGH][IJKL][MN~pad~~pad~]
+        #
+        # ## pass 0 - block 0 ##
+        # so we feed in "AAA" so oracle encrypts:
+        # [AAAX][BCDE][FGHI][JKLM][N~pad~~pad~~pad~]
+        # we keep this ciphertext for comparison
+        # then we feed in "AAA-chr(0-255)-" so oracle encrypts:
+        # [AAA-chr(0-255)-][XBCD][EFGH][IJKL][MN~pad~~pad~]
+        # we compare each iteration with the kept ctext from above
+        # if the first block matches, we know that the first oracle added character is one of the values,
+        # in this instance X
+        #
+        #  ## pass 1 - block 0
+        # we feed in "AA" and know the next char is X so oracle encrypts
+        # [AAXB][CDEF][GHIJ][KLMN]  <- note, 1 block less - would be a full padded block otherwise
+        # we keep this ciphertext for comparison
+        # then we feed in "AAX-chr(0-255)-" so oracle encrypts:
+        # [AAX-chr(0-255)-][XBCD][EFGH][IJKL][MN~pad~~pad~]
+        # we compare each iteration with the kept ctext from above
+        # if the first block matches, we know that the second oracle added character is one of the values,
+        # in this instance B
+        #
+        #  ## pass 2 - block 0
+        # we feed in "A" and know the next chars are XB so oracle encrypts
+        # [AXBC][DEFG][HIJK][LMN~pad~]  <- note, 1 block less - would be a full padded block otherwise
+        # we keep this ciphertext for comparison
+        # then we feed in "AXB-chr(0-255)-" so oracle encrypts:
+        # [AXB-chr(0-255)-][XBCD][EFGH][IJKL][MN~pad~~pad~]
+        # we compare each iteration with the kept ctext from above
+        # if the first block matches, we know that the third oracle added character is one of the values,
+        # in this instance C
+        #
+        #  ## pass 3 - block 0
+        # we feed in "" and know the next chars are XBC so oracle encrypts
+        # [XBCD][EFGH][IJKL][MN~pad~~pad~]  <- note, 1 block less - would be a full padded block otherwise
+        # we keep this ciphertext for comparison
+        # then we feed in "XBC-chr(0-255)-" so oracle encrypts:
+        # [XBC-chr(0-255)-][XBCD][EFGH][IJKL][MN~pad~~pad~]
+        # we compare each iteration with the kept ctext from above
+        # if the first block matches, we know that the fourth oracle added character is one of the values,
+        # in this instance D
+        #
+        ######
+        # ## we now know the first block in entirety - onto the second block
+        ######
+        #
+        #  ## pass 0 - block 1
+        # so we feed in "AAA" so oracle encrypts:
+        # [AAAX][BCDE][FGHI][JKLM][N~pad~~pad~~pad~]
+        # note that we are focussing attention on the second block now
+        # we know from this 'new' second block that the first 3 chars are "BCD"
+        # we keep this ciphertext for comparison
+        # then we feed in "BCD-chr(0-255)-" so oracle encrypts:
+        # [BCD-chr(0-255)-][XBCD][EFGH][IJKL][MN~pad~~pad~]
+        # we compare each iteration with the kept ctext from above
+        # if the first block of this matches the second block above, we know that the 5th oracle added character is
+        # in this instance E
+        #
+        #  ## and repeat
         # so - here goes...
-        # identify the number of blocks generated when calling the oracle with an empty string
-        ptext_blocks = []
-        num_ctext_blocks = blank_ctlen // blocklen
-        # iterate over each block to solve
-        for i in range(num_ctext_blocks):
-            ptext = bytearray()
-            # obtain the block to solve plus the subsequent block
-            # NOTE: need to do someting later here - haven't handled the case for the last block
-            blank_block = blank_ctext[i*blocklen:(i+1)*blocklen]
 
-            # now iterate over the length of the block
+        num_ctext_blocks = blank_ctlen // blocklen
+        ptext = bytearray()
+        for i in range(num_ctext_blocks):
+            # determine the string we are going to use to compare with
+            # for the 0th block, it's the synthetic set of As
+            # for block > 0 it's the prior ptext block
+            if(i == 0): compare_block = bytearray(blocklen * "A", 'utf-8')
+            else: compare_block = ptext[(i-1)*blocklen:i*blocklen]
 
             for j in range(blocklen):
-                feedlen = blocklen - j - 1 - len(ptext)
-                prepend_str = feedlen * "A"
-                prepend_ba = bytearray(prepend_str, 'utf-8')
-                prepend_ba += ptext
-                test_prepend_ba = bytearray()
-                test_prepend_ba[:] = prepend_ba[:]
-
-                pullback_ctext = ciphers.aes128ecb_enc_oracle(prepend_ba)
+                # the feed string into the oracle is made up of "A" padding
+                padding_len = blocklen - j - 1
+                feedstr = bytearray(padding_len * "A", 'utf-8')
+                pullback_ctext = ciphers.aes128ecb_enc_oracle(feedstr)
 
                 for k in range(255):
-                    test_prepend_ba.append(k)
-                    print(test_prepend_ba)
-                    compare_ctext = ciphers.aes128ecb_enc_oracle(test_prepend_ba)
-                    if compare_ctext[0:blocklen] == pullback_ctext[0:blocklen]:
-                        ptext.append(k)
-                        print(chr(k))
-                        break
 
-            ptext_blocks.append(ptext)
+                    # this area is causing all the grief
+                    if j == 15: current_compare_block = ptext[-j:] + bytearray(chr(k), 'utf-8')
+                    elif j == 0: current_compare_block = compare_block[-(blocklen-j-1):] + bytearray(chr(k), 'utf-8')
+                    else: current_compare_block = compare_block[-(blocklen-j-1):] + ptext[-j:] + bytearray(chr(k), 'utf-8')
+
+                    test_ctext = ciphers.aes128ecb_enc_oracle(current_compare_block)
+                    standalone_test_block = test_ctext[0:blocklen]
+                    scan_pullback_test_block = pullback_ctext[i*blocklen:(i+1)*blocklen]
+                    if  standalone_test_block == scan_pullback_test_block:
+                        ptext.append(k)
+                        break
+                    else:
+                        continue
+        print(ptext)
